d2fb2802d12dadd1690e6051bb19957bbd45b9b1
mohammadg <webmaster@mohammadg.com>
2015-03-03 22:12:31 +1100

    .

--- app.js
+++ app.js
@@ -5,10 +5,15 @@ app.controller("VersionController", function($scope, $http) {
   $scope.deployments = [];
   
   //Retrieve all build versions
-  var data = $http.get('json/versions.json');
+  var json;
+  
+  $http.get('json/versions.json').
+  success(function(data, status, headers, config) {
+    json = data;
+  });
 
   //Push required fields onto array
-  data.forEach(function(item1) {
+  json.forEach(function(item1) {
     item1.links.forEach(function(item2) {
       $scope.deployments.push({
         short_descr: item1.short_descr,
