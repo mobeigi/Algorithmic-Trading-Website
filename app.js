fe4449b9e528cd0c8829d710d5c506fc760bb5a1
mohammadg <webmaster@mohammadg.com>
2015-03-03 13:22:59 +1100

    Changed version.json to be valid json

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-(function(angular) {
-  'use strict';
-
-var app = angular.module('appDownload', []);
+var app = angular.module("appDownload", []);
 
 app.controller("VersionController", function($scope, $http) {
-    $http.get('http://www.w3schools.com/website/Customers_JSON.php').success(function(data, status, headers, config) {$scope.test = data;});
+  $http.get('http://www.w3schools.com/website/Customers_JSON.php').
+    success(function(data, status, headers, config) {
+      $scope.deployments = data;
+    });
 });
