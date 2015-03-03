b8b854becb926428e410072d6d5a25e90e9bdef7
mohammadg <webmaster@mohammadg.com>
2015-03-03 22:07:16 +1100

    Rewrite JSON parse approach

--- app.js
+++ app.js
@@ -1,8 +1,21 @@
 var app = angular.module("appDownload", []);
 
 app.controller("VersionController", function($scope, $http) {
-  $http.get('json/versions.json').
-    success(function(data, status, headers, config) {
-      $scope.deployments = data;
+
+  $scope.deployments = [];
+  
+  //Retrieve all build versions
+  var data = $http.get('json/versions.json');
+
+  //Push required fields onto array
+  data.forEach(function(item1) {
+    item1.links.forEach(function(item2) {
+      $scope.deployments.push({
+        short_descr: item1.short_descr,
+        link: item2.link,
+        os: item2.os
+      });
     });
+  })
+  
 });
