6b0e155457d32aa746d08fda167d659095040e00
mohammadg <webmaster@mohammadg.com>
2015-03-04 21:44:52 +1100

    .

--- js/app.js
+++ js/app.js
@@ -36,7 +36,29 @@ app.controller("ViewerController", function($scope, $http, $sce, $compile) {
     $http.get("includes/".concat(webpageName)).
     success(function(data, status, headers, config) {
       $scope.content = $sce.trustAsHtml(data);
-      $compile($scope.content)($scope);
+      //$compile($scope.content)($scope);
     });
   };
 });
+
+app.directive('compile', ['$compile', function ($compile) {
+    return function(scope, element, attrs) {
+        scope.$watch(
+            function(scope) {
+                // watch the 'compile' expression for changes
+                return scope.$eval(attrs.compile);
+            },
+            function(value) {
+                // when the 'compile' expression changes
+                // assign it into the current DOM
+                element.html(value);
+
+                // compile the new DOM and link it to the current
+                // scope.
+                // NOTE: we only compile .childNodes so that
+                // we don't get into infinite loop compiling ourselves
+                $compile(element.contents())(scope);
+            }
+        );
+    };
+}])
