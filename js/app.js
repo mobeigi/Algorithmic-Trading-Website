a2846d6bd412f6f0e55c9fba7ef21cfc07fc8ffe
mohammadg <webmaster@mohammadg.com>
2015-03-04 22:03:26 +1100

    .

--- js/app.js
+++ js/app.js
@@ -43,15 +43,14 @@ app.controller("ViewerController", function($scope, $http, $sce, $compile) {
 });
 
 app.directive('compile', function ($compile) {
-	  return {
-	    restrict: 'A',
-	    replace: true,
-	    link: function (scope, ele, attrs) {
-	      scope.$watch(attrs.dynamic, function(html) {
-	        ele.html(html);
-	        $compile(ele.contents())(scope);
-	      });
-	    }
-	  };
-	});
-}])
+  return {
+    restrict: 'A',
+    replace: true,
+    link: function (scope, ele, attrs) {
+      scope.$watch(attrs.dynamic, function(html) {
+        ele.html(html);
+        $compile(ele.contents())(scope);
+      });
+    }
+  };
+});
