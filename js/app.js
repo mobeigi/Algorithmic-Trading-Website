9ba98c9829ae7a638f7044e6247c9a52acb96347
mohammadg <webmaster@mohammadg.com>
2015-03-04 20:11:49 +1100

    Add error reporting

--- js/app.js
+++ js/app.js
@@ -37,6 +37,11 @@ app.controller("ViewerController", function($scope, $http, $document) {
     success(function(data, status, headers, config) {
       var contentElement = $document.find('#content');
       contentElement.replaceWith(data);
+    }).
+    error(function(data, status, headers, config) {
+      // called asynchronously if an error occurs
+      // or server returns response with an error status.
+      console.log(status);
     });
   };
 });
